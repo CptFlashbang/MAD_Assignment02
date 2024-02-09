53d970bc5d950e619d11ea65d512db41bc641115
Callum Yates <47091219+CptFlashbang@users.noreply.github.com>
2024-02-09 12:07:27 +0000

    Added composable FormattedPriceLabel for commonly used formatted prices

--- app/src/main/java/com/example/mad_assignment02/ui/component/CommonUI.kt
+++ app/src/main/java/com/example/mad_assignment02/ui/component/CommonUI.kt
@@ -1,4 +1,20 @@
 package com.example.mad_assignment02.ui.component
 
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.example.mad_assignment02.R
+
 class CommonUI {
+    @Composable
+    fun FormattedPriceLabel(subtotal: String, modifier: Modifier = Modifier) {
+        Text(
+            text = stringResource(R.string.subtotal_price, subtotal),
+            modifier = modifier,
+            style = MaterialTheme.typography.headlineSmall
+        )
+    }
+
 }
