66819c38073584f42219793b9625539d2774757a
Callum Yates <47091219+CptFlashbang@users.noreply.github.com>
2024-02-09 20:35:15 +0000

    Refactor. Moved composables outside of class

--- app/src/main/java/com/example/mad_assignment02/ui/screen/ReadyMadeScreen.kt
+++ app/src/main/java/com/example/mad_assignment02/ui/screen/ReadyMadeScreen.kt
@@ -65,3 +65,46 @@ class ReadyMadeScreen {
         }
     }
 }
+
+@Composable
+fun Ready_Made_List_Item(burrito: Burrito) {
+    androidx.compose.material3.ListItem(
+        headlineText = {
+            Text(text = stringResource(burrito.title))
+        },
+        supportingText = {
+            Text(text = stringResource(burrito.mainFilling))
+        },
+        trailingContent = { FormattedPriceLabel(burrito.price) }
+    )
+}
+@Composable
+fun ReadyMadeList() {
+    val readyMadeBurritos = DataSource.loadReadyMade()
+
+    LazyColumn {
+        items(readyMadeBurritos.size) { index ->
+            Ready_Made_List_Item(readyMadeBurritos[index])
+            Divider()
+        }
+    }
+}
+@Composable
+fun Ready_Made_Burrito(){
+    Scaffold(
+        bottomBar = {
+            BottomAppBar(
+            ) {
+                BottomNavBar()
+            }
+        },
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+        ) {
+            ReadyMadeList()
+        }
+    }
+}
