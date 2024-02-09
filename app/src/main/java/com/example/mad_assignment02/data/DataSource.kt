746384f3f2654cb9d60fb6122f4e67beaaea0ffb
Callum Yates <47091219+CptFlashbang@users.noreply.github.com>
2024-02-09 12:25:53 +0000

    Corrected list name to be more accurate to the list contents in DataSource.kt

--- app/src/main/java/com/example/mad_assignment02/data/DataSource.kt
+++ app/src/main/java/com/example/mad_assignment02/data/DataSource.kt
@@ -1,4 +1,19 @@
 package com.example.mad_assignment02.data
 
+import com.example.mad_assignment02.R
+
 object DataSource {
+    val main_fillings = listOf(
+        R.string.Main_Filling_01,
+        R.string.Main_Filling_02,
+        R.string.Main_Filling_03,
+        R.string.Main_Filling_04,
+        R.string.Main_Filling_05,
+        R.string.Main_Filling_06,
+        R.string.Main_Filling_07,
+        R.string.Main_Filling_08,
+        R.string.Main_Filling_09,
+        R.string.Main_Filling_10,
+    )
+
 }
