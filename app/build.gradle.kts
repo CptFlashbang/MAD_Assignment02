b50c0cbb6297cfaddf64c578da57c969a04f3cce
Callum Yates <47091219+CptFlashbang@users.noreply.github.com>
2024-02-09 11:40:38 +0000

    Added dependency for GSON (JSON file reading)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,4 +69,5 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.7.6")
     implementation("androidx.compose.material:material:1.6.0")
     implementation("androidx.compose.material3:material3-window-size-class")
+    implementation("com.google.code.gson:gson:2.8.9")
 }
