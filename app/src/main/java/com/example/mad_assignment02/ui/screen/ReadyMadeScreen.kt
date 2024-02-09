0e8dd40e53b98c4328f6ec65107f84337857438c
Callum Yates <47091219+CptFlashbang@users.noreply.github.com>
2024-02-09 18:12:46 +0000

    Added Ready_Made_List_Item composable to ReadyMadeScreen.kt

--- app/src/main/java/com/example/mad_assignment02/ui/screen/ReadyMadeScreen.kt
+++ app/src/main/java/com/example/mad_assignment02/ui/screen/ReadyMadeScreen.kt
@@ -4,9 +4,26 @@ package com.example.mad_assignment02.ui.screen
 
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import com.example.mad_assignment02.data.Burrito
+import com.example.mad_assignment02.data.DataSource
 
 class ReadyMadeScreen {
+    @Composable
+    fun Ready_Made_List_Item(burrito: Burrito) {
+        androidx.compose.material3.ListItem(
+            headlineText = {
+                Text(text = stringResource(burrito.title))
+            },
+            supportingText = {
+                Text(text = stringResource(burrito.mainFilling))
+            },
+            trailingContent = { Text(text = "£${burrito.price}") }
+        )
+    }
     @Composable
     fun ReadyMadeList() {
         val readyMadeBurritos = Datasource().loadReadyMade()
