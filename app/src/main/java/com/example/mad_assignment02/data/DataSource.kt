93f1d274f233596f7ff92a0b878a77b4dc561749
Callum Yates <47091219+CptFlashbang@users.noreply.github.com>
2024-02-09 18:05:15 +0000

    Moved Burrito class outside of DataSource object

--- app/src/main/java/com/example/mad_assignment02/data/DataSource.kt
+++ app/src/main/java/com/example/mad_assignment02/data/DataSource.kt
@@ -111,54 +111,53 @@ object DataSource {
             )
         )
     }
+}
+data class Burrito(
+    @StringRes val title: Int,
+    @StringRes val description: Int,
+    val price: Double,
+    val mainFilling: Int,
+    val additionalFillings: List<Int>,
+    val sauces: List<Int>,
+    val salads: List<Int>
+) {
+    companion object {
+        fun createReadyMade(
+            @StringRes title: Int,
+            @StringRes description: Int,
+            price: Double,
+            mainFilling: Int,
+            additionalFillings: List<Int>,
+            sauces: List<Int>,
+            salads: List<Int>
+        ): Burrito {
+            return Burrito(
+                title,
+                description,
+                price,
+                mainFilling,
+                additionalFillings,
+                sauces,
+                salads
+            )
+        }
 
-    data class Burrito(
-        @StringRes val title: Int,
-        @StringRes val description: Int,
-        val price: Double,
-        val mainFilling: Int,
-        val additionalFillings: List<Int>,
-        val sauces: List<Int>,
-        val salads: List<Int>
-    ) {
-        companion object {
-            fun createReadyMade(
-                @StringRes title: Int,
-                @StringRes description: Int,
-                price: Double,
-                mainFilling: Int,
-                additionalFillings: List<Int>,
-                sauces: List<Int>,
-                salads: List<Int>
-            ): Burrito {
-                return Burrito(
-                    title,
-                    description,
-                    price,
-                    mainFilling,
-                    additionalFillings,
-                    sauces,
-                    salads
-                )
-            }
-
-            fun createCustom(
-                price: Double,
-                mainFilling: Int,
-                additionalFillings: List<Int>,
-                sauces: List<Int>,
-                salads: List<Int>
-            ): Burrito {
-                return Burrito(
-                    title = 0, // placeholder default value necessary for custom
-                    description = 0, // placeholder default value necessary for custom
-                    price,
-                    mainFilling,
-                    additionalFillings,
-                    sauces,
-                    salads
-                )
-            }
+        fun createCustom(
+            price: Double,
+            mainFilling: Int,
+            additionalFillings: List<Int>,
+            sauces: List<Int>,
+            salads: List<Int>
+        ): Burrito {
+            return Burrito(
+                title = 0, // placeholder default value necessary for custom
+                description = 0, // placeholder default value necessary for custom
+                price,
+                mainFilling,
+                additionalFillings,
+                sauces,
+                salads
+            )
         }
     }
 }
