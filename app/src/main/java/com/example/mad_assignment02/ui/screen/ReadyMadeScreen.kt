5f4717ff3c298a750e90d9610cd629ea00ffdf1a
Callum Yates <47091219+CptFlashbang@users.noreply.github.com>
2024-02-09 18:01:31 +0000

    Replaced misc file with Kotlin class file

---
+++ app/src/main/java/com/example/mad_assignment02/ui/screen/ReadyMadeScreen.kt
@@ -0,0 +1,19 @@
+package com.example.mad_assignment02.ui.screen
+
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Divider
+import androidx.compose.runtime.Composable
+
+class ReadyMadeScreen {
+    @Composable
+    fun ReadyMadeList() {
+        val readyMadeBurritos = Datasource().loadReadyMade()
+
+        LazyColumn {
+            items(readyMadeBurritos.size) { index ->
+                Ready_Made_List_Item(readyMadeBurritos[index])
+                Divider()
+            }
+        }
+    }
+}
