f0c88265ebe482941c5537ff319468c9c99a0cf9
Callum Yates <47091219+CptFlashbang@users.noreply.github.com>
2024-02-09 19:10:31 +0000

    Added scaffold for screen composable

--- app/src/main/java/com/example/mad_assignment02/ui/screen/ReadyMadeScreen.kt
+++ app/src/main/java/com/example/mad_assignment02/ui/screen/ReadyMadeScreen.kt
@@ -1,15 +1,25 @@
-@file:OptIn(ExperimentalMaterial3Api::class)
+@file:OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterial3Api::class,
+    ExperimentalMaterial3Api::class
+)
 
 package com.example.mad_assignment02.ui.screen
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.example.mad_assignment02.data.Burrito
 import com.example.mad_assignment02.data.DataSource
+import com.example.mad_assignment02.ui.component.BottomNavBar
 
 class ReadyMadeScreen {
     @Composable
@@ -32,6 +42,25 @@ class ReadyMadeScreen {
             items(readyMadeBurritos.size) { index ->
                 Ready_Made_List_Item(readyMadeBurritos[index])
                 Divider()
+            }
+        }
+    }
+    @Composable
+    fun Ready_Made_Burrito(){
+        Scaffold(
+            bottomBar = {
+                BottomAppBar(
+                ) {
+                    BottomNavBar()
+                }
+            },
+        ) { innerPadding ->
+            Column(
+                modifier = Modifier
+                    .padding(innerPadding),
+                verticalArrangement = Arrangement.spacedBy(16.dp),
+            ) {
+                ReadyMadeList()
             }
         }
     }
