719424b6d12dc8a7de2c5e5078d2f0babe1afce2
Callum Yates <47091219+CptFlashbang@users.noreply.github.com>
2024-02-09 13:33:46 +0000

    Added navbar composable to common items package

---
+++ app/src/main/java/com/example/mad_assignment02/ui/component/NavBar.kt
@@ -0,0 +1,38 @@
+package com.example.mad_assignment02.ui.component
+
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import com.example.mad_assignment02.R
+
+class NavBar {
+    @Composable
+    fun BottomNavBar(){
+        var selectedItem by remember { mutableStateOf(0) }
+        val items: List<Pair<String, Painter>> = listOf(
+            "Home" to painterResource(id = R.drawable.home_fill0_wght400_grad0_opsz24),
+            "Ready-made" to painterResource(id = R.drawable.fastfood_fill0_wght400_grad0_opsz24),
+            "Custom" to painterResource(id = R.drawable.build_fill0_wght400_grad0_opsz24),
+            "Orders" to painterResource(id = R.drawable.assignment_fill0_wght400_grad0_opsz24),
+        )
+
+        androidx.compose.material3.NavigationBar {
+            items.forEachIndexed { index, (label, painter) ->
+                NavigationBarItem(
+                    icon = { Icon(painter, contentDescription = label) },
+                    label = { Text(label) },
+                    selected = selectedItem == index,
+                    onClick = { selectedItem = index }
+                )
+
+            }
+        }
+    }
+}
