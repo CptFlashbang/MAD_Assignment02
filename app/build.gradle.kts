6268ebcf76b3498756b9881c25205ccb19d327ff
Callum Yates <47091219+CptFlashbang@users.noreply.github.com>
2024-02-09 11:41:03 +0000

    Added dependency for UI State handling

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,4 +70,5 @@ dependencies {
     implementation("androidx.compose.material:material:1.6.0")
     implementation("androidx.compose.material3:material3-window-size-class")
     implementation("com.google.code.gson:gson:2.8.9")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
 }
