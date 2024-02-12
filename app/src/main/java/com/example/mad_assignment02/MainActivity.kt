41dd311151930e4a3a8376205b8fe687b5837e44
Callum Yates <47091219+CptFlashbang@users.noreply.github.com>
2024-02-12 00:53:50 +0000

    updated import files

--- app/src/main/java/com/example/mad_assignment02/MainActivity.kt
+++ app/src/main/java/com/example/mad_assignment02/MainActivity.kt
@@ -1,16 +1,33 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.mad_assignment02
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.ArrowBack
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.mad_assignment02.ui.screen.ReadyMadeMasterScreen
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.mad_assignment02.ui.component.BottomNavBar
+import com.example.mad_assignment02.ui.screen.Custom_Screen
+import com.example.mad_assignment02.ui.screen.Home_Screen
+import com.example.mad_assignment02.ui.screen.Order_Screen
 import com.example.mad_assignment02.ui.screen.Ready_Made_Master_Screen
 import com.example.mad_assignment02.ui.theme.MAD_Assignment02Theme
 
