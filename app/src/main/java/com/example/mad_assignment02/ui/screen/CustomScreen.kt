8009f0ddb36a03f939276a92377a29956c43dac4
Callum Yates <47091219+CptFlashbang@users.noreply.github.com>
2024-02-13 00:14:09 +0000

    Added additional composable for individual filling composables

--- app/src/main/java/com/example/mad_assignment02/ui/screen/CustomScreen.kt
+++ app/src/main/java/com/example/mad_assignment02/ui/screen/CustomScreen.kt
@@ -7,10 +7,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.example.mad_assignment02.ui.component.BottomNavBar
@@ -21,12 +24,30 @@ class CustomScreen {
 @Preview
 @Composable
 fun CustomScreenPreview() {
-    Main_Fillings()
+    Main_Filling()
 }
 
 @Composable
-fun Main_Fillings(){
+fun Main_Filling(){
     androidx.compose.material3.ListItem(
+        leadingContent = {
+
+        },
+        headlineText = { /*TODO*/ }
+    )
+}
+@Composable
+fun Main_Fillings_List(){
+    androidx.compose.material3.ListItem(
+        leadingContent = {
+            RadioButton(
+                selected = selectedValue == item,
+                onClick = {
+                    selectedValue = item
+                    onSelectionChanged(item)
+                }
+
+        },
         headlineText = { /*TODO*/ }
     )
 
