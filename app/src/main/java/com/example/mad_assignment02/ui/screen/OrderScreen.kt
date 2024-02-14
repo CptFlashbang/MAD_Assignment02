28a7688c61f5810f5c396f876b61caa9ac5b13aa
Callum Yates <47091219+CptFlashbang@users.noreply.github.com>
2024-02-14 10:28:35 +0000

    Added tab row, and variable for it to track

--- app/src/main/java/com/example/mad_assignment02/ui/screen/OrderScreen.kt
+++ app/src/main/java/com/example/mad_assignment02/ui/screen/OrderScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -34,5 +35,8 @@ fun Order_Screen(){
 
 @Composable
 fun TopTabs(){
+    var selectedTabIndex:Int =1
+    TabRow(selectedTabIndex) {
 
+    }
 }
