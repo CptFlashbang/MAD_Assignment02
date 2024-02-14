69be336c77354eeef931a9e9b2c9381d89ec5a14
Callum Yates <47091219+CptFlashbang@users.noreply.github.com>
2024-02-14 10:46:03 +0000

    Added preview for component

--- app/src/main/java/com/example/mad_assignment02/ui/screen/OrderScreen.kt
+++ app/src/main/java/com/example/mad_assignment02/ui/screen/OrderScreen.kt
@@ -1,4 +1,4 @@
-@file:OptIn(ExperimentalMaterial3Api::class)
+@file:OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterial3Api::class)
 
 package com.example.mad_assignment02.ui.screen
 
@@ -8,15 +8,23 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.example.mad_assignment02.ui.component.BottomNavBar
 
 class OrderScreen {
+}
+
+@Preview
+@Composable
+fun Order_Screen_Preview() {
+    TopTabs()
 }
 @Composable
 fun Order_Screen(){
