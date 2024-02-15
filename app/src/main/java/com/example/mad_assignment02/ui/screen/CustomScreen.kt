a3f8c94128e97e4eff33a94ab32bd7c4220787a8
Callum Yates <47091219+CptFlashbang@users.noreply.github.com>
2024-02-15 00:55:59 +0000

    Added Mutable int to track selected MainFilling to parameters. Allows to move the responsibility to parent composable

--- app/src/main/java/com/example/mad_assignment02/ui/screen/CustomScreen.kt
+++ app/src/main/java/com/example/mad_assignment02/ui/screen/CustomScreen.kt
@@ -75,7 +75,7 @@ fun MainFillingListItem(fillingText: String, isSelected: Boolean, onSelect: () -
 }
 
 @Composable
-fun MainFillingsSection(mainFillings: List<Int>) {
+fun MainFillingsSection(mainFillings: List<Int>, selectedMainFilling: MutableState<Int>) {
     var selectedFilling by remember { mutableStateOf(mainFillings.firstOrNull() ?: 0) }
 
     Column {
