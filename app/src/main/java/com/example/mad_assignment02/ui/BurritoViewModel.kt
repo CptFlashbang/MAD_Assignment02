c9e7232651b4de41e4fb6fba36091a1c6c0c1951
Callum Yates <47091219+CptFlashbang@users.noreply.github.com>
2024-02-18 20:15:32 +0000

    Updated to work with UI state

--- app/src/main/java/com/example/mad_assignment02/ui/BurritoViewModel.kt
+++ app/src/main/java/com/example/mad_assignment02/ui/BurritoViewModel.kt
@@ -1,17 +1,23 @@
 package com.example.mad_assignment02.ui
 
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.mad_assignment02.data.BurritoClass
 import com.example.mad_assignment02.data.DataSource.ReadyMadeBurritos
+import com.example.mad_assignment02.ui.UI_States.OrderUIState
 
 class BurritoViewModel : ViewModel()  {
-    private val _currentOrder = MutableLiveData<List<BurritoClass>>(listOf())
-    val currentOrder: LiveData<List<BurritoClass>> = _currentOrder
+//    private val _currentOrder = MutableLiveData<List<BurritoClass>>(listOf())
+//    val currentOrder: LiveData<List<BurritoClass>> = _currentOrder
+    private val _uiState = mutableStateOf(OrderUIState())
+    val uiState: State<OrderUIState> = _uiState
 
     fun addToOrder(burrito: BurritoClass) {
-        _currentOrder.value = _currentOrder.value.orEmpty() + burrito
+//        _currentOrder.value = _currentOrder.value.orEmpty() + burrito
+        _uiState.value = OrderUIState(_uiState.value.burritos + burrito)
     }
 
     fun getBurritoById(id: Int): BurritoClass {
