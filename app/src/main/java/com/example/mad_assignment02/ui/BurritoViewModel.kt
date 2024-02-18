4569b07a64de07a40a10f121ef82f1d86f13987b
Callum Yates <47091219+CptFlashbang@users.noreply.github.com>
2024-02-18 18:00:07 +0000

    Added private and public attributes for tracking and amending current order

--- app/src/main/java/com/example/mad_assignment02/ui/BurritoViewModel.kt
+++ app/src/main/java/com/example/mad_assignment02/ui/BurritoViewModel.kt
@@ -1,10 +1,14 @@
 package com.example.mad_assignment02.ui
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.mad_assignment02.data.BurritoClass
 import com.example.mad_assignment02.data.DataSource.ReadyMadeBurritos
 
 class BurritoViewModel : ViewModel()  {
+    private val _currentOrder = MutableLiveData<List<BurritoClass>>(listOf())
+    val currentOrder: LiveData<List<BurritoClass>> = _currentOrder
     fun getBurritoById(id: Int): BurritoClass {
         return ReadyMadeBurritos.first { it.id == id }
     }
