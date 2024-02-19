a0002c1a1bc1ffe7cc63bd8a9fd10671afce3e9c
Callum Yates <47091219+CptFlashbang@users.noreply.github.com>
2024-02-19 17:30:15 +0000

    Added home screen content

--- app/src/main/java/com/example/mad_assignment02/ui/screen/HomeScreen.kt
+++ app/src/main/java/com/example/mad_assignment02/ui/screen/HomeScreen.kt
@@ -2,17 +2,24 @@
 
 package com.example.mad_assignment02.ui.screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.example.mad_assignment02.R
 import com.example.mad_assignment02.ui.component.BottomNavBar
 
 class HomeScreen {
@@ -27,7 +34,34 @@ fun Home_Screen(){
                 .padding(innerPadding),
             verticalArrangement = Arrangement.spacedBy(16.dp),
         ) {
-            Text(text = "Home_Screen")
+            LazyColumnExample()
+        }
+    }
+}
+
+
+@Composable
+fun LazyColumnExample() {
+    val stringIds = listOf(
+        R.string.HomeScreenSectionTitle1,
+        R.string.HomeScreenSection1,
+        R.string.HomeScreenSection2,
+        R.string.HomeScreenSection3,
+        R.string.HomeScreenSectionTitle2,
+        R.string.HomeScreenSubSectionTitle1,
+        R.string.HomeScreenSection4,
+        R.string.HomeScreenSection5,
+        R.string.HomeScreenSubSectionTitle2,
+        R.string.HomeScreenSection6,
+        R.string.HomeScreenSection7
+    )
+
+    LazyColumn(modifier = Modifier.fillMaxSize()) {
+        item {
+            Image(painter = painterResource(id = R.drawable.sangria_senorial_ah2fCJP9Eok_unsplash), contentDescription = "Sangria Image")
+        }
+        items(stringIds) { stringId ->
+            Text(text = stringResource(id = stringId))
         }
     }
 }
