bcb533d608ab6d40fd3e10c031bcd3b44332e88d
Callum Yates <47091219+CptFlashbang@users.noreply.github.com>
2024-02-20 02:06:40 +0000

    refactored home screen composable to add stylings

--- app/src/main/java/com/example/mad_assignment02/ui/screen/HomeScreen.kt
+++ app/src/main/java/com/example/mad_assignment02/ui/screen/HomeScreen.kt
@@ -9,59 +9,63 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.example.mad_assignment02.R
 import com.example.mad_assignment02.ui.component.BottomNavBar
-
-class HomeScreen {
-}
 @Composable
-fun Home_Screen(){
-    Scaffold(
-
-    ) { innerPadding ->
-        Column(
-            modifier = Modifier
-                .padding(innerPadding),
-            verticalArrangement = Arrangement.spacedBy(16.dp),
-        ) {
-            LazyColumnExample()
-        }
-    }
-}
-
-
-@Composable
-fun LazyColumnExample() {
-    val stringIds = listOf(
-        R.string.HomeScreenSectionTitle1,
-        R.string.HomeScreenSection1,
-        R.string.HomeScreenSection2,
-        R.string.HomeScreenSection3,
-        R.string.HomeScreenSectionTitle2,
-        R.string.HomeScreenSubSectionTitle1,
-        R.string.HomeScreenSection4,
-        R.string.HomeScreenSection5,
-        R.string.HomeScreenSubSectionTitle2,
-        R.string.HomeScreenSection6,
-        R.string.HomeScreenSection7
-    )
-
-    LazyColumn(modifier = Modifier.fillMaxSize()) {
+fun Home_Screen() {
+    LazyColumn(
+        modifier = Modifier.padding(horizontal = 16.dp, vertical = 16.dp),
+        verticalArrangement = Arrangement.spacedBy(32.dp)
+    ) {
         item {
-            Image(painter = painterResource(id = R.drawable.burrito_hero), contentDescription = "Sangria Image")
+            Image(
+                painter = painterResource(id = R.drawable.burrito_hero),
+                contentDescription = "Sangria Image",
+                modifier = Modifier.clip(RoundedCornerShape(28.dp))
+            )
         }
-        items(stringIds) { stringId ->
-            Text(text = stringResource(id = stringId))
+        item {
+            Column(verticalArrangement = Arrangement.spacedBy(16.dp))  {
+                Text(text = stringResource(id = R.string.HomeScreenSectionTitle1), style = MaterialTheme.typography.headlineMedium)
+                Text(text = stringResource(id = R.string.HomeScreenSection1), style = MaterialTheme.typography.bodyMedium)
+                Text(text = stringResource(id = R.string.HomeScreenSection2), style = MaterialTheme.typography.bodyMedium)
+                Text(text = stringResource(id = R.string.HomeScreenSection3), style = MaterialTheme.typography.bodyMedium)
+            }
+        }
+        item {
+            Divider()
+        }
+        item {
+            Column(verticalArrangement = Arrangement.spacedBy(16.dp))  {
+                Text(text = stringResource(id = R.string.HomeScreenSectionTitle2), style = MaterialTheme.typography.headlineSmall)
+                Text(text = stringResource(id = R.string.HomeScreenSubSectionTitle1), style = MaterialTheme.typography.titleLarge)
+                Text(text = stringResource(id = R.string.HomeScreenSection4), style = MaterialTheme.typography.bodyMedium)
+                Text(text = stringResource(id = R.string.HomeScreenSection5), style = MaterialTheme.typography.bodyMedium)
+            }
+        }
+        item {
+            Divider()
+        }
+        item {
+            Column(verticalArrangement = Arrangement.spacedBy(16.dp)) {
+                Text(text = stringResource(id = R.string.HomeScreenSubSectionTitle2), style = MaterialTheme.typography.titleLarge)
+                Text(text = stringResource(id = R.string.HomeScreenSection6), style = MaterialTheme.typography.bodyMedium)
+                Text(text = stringResource(id = R.string.HomeScreenSection7), style = MaterialTheme.typography.bodyMedium)
+            }
         }
     }
 }
